7632dfc9604d1281b9500b15aaa127f456ddd0d2
GaboAfk <gabrielmorffe95@gmail.com>
2024-04-01 16:45:30 -0300

    header update

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-binary_tree_node.c
+ * Auth: Gabriel Morffe
+ */
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_t *binary_tree_node(binary_tree_t *parent, int value) - function that creates a binary tree node
+ *
+ * Return: Always 0 (Success)
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+/*yourcodebitch*/
+	return (0);
+}
