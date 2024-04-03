4643bf1fd693a3c9c79aee26815eca2ba896ff1c
GaboAfk <gabrielmorffe95@gmail.com>
2024-04-02 21:52:16 -0300

    final update

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,10 +15,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree)
 		return (0);
-	if (!tree->left ^ !tree->right)
-		return (1);
 
-	/*se debe de tomar en cuenta si un hijo solo tiene hijos a ese lado*/
-	/*return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));*/
-	return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left || tree->right)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
+		return (0);
 }
