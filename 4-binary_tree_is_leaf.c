3667cdb16857cd0bc51702ebe92f05526855ec36
balemansteve <balemansteve@gmail.com>
2024-04-04 18:27:27 +0000

    binary tree is leaf update

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -15,10 +15,10 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	if (!node)
 		return (0);
 	if (node->left || node->right)
-		return (1);
+		return (0);
 
 	else
 	{
-		return (0);
+		return (1);
 	}
 }
