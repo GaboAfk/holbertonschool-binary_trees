d0068e6c8a1e4134c56d8e1594ea2be9f87c782b
balemansteve <balemansteve@gmail.com>
2024-04-09 18:42:00 +0000

    tree is full fix

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -14,12 +14,22 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int left = 0, right = 0;
+
 	if (!tree)
 		return (0);
 
-	if (tree->left && tree->right && tree->parent)
-		return (1);
+	if (tree->left)
+		left = 1 + binary_tree_is_full(tree->left);
 
 	else
 		return (0);
+
+	if (tree->right)
+		right = 1 + binary_tree_is_full(tree->right);
+
+	else
+		return (0);
+
+	return (left == right);
 }
