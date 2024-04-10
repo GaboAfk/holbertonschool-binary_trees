129773b736afa228f654890ae0d93e3cff76702c
GaboAfk <gabrielmorffe95@gmail.com>
2024-04-10 16:52:31 -0300

    reference pointer in aux function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,23 +8,31 @@
 /**
  * perfect_aux - checks if a binary tree is perfect
  * @tree: is a pointer to the root node of the tree to check
- *
+ * @full: flag of each full tree
  * Return: 1 tree is perfectf, else 0, if tree is NULL, return 0
  */
-int perfect_aux(const binary_tree_t *tree)
+int perfect_aux(const binary_tree_t *tree, int **full)
 {
 	int left = 0, right = 0;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 
+	if ((!tree->left && tree->right) || (tree->left && !tree->right))
+	{
+		*full = 0;
+		return (0);
+	}
+
 	if (tree->left)
-		left += 1 + perfect_aux(tree->left);
+		left += 1 + perfect_aux(tree->left, full);
 
 	if (tree->right)
-		right += 1 + perfect_aux(tree->right);
+		right += 1 + perfect_aux(tree->right, full);
 
-	return (left > right ? left : right);
+	if (*full)
+		return (left > right ? left : right);
+	return (0);
 }
 
 /*
@@ -42,8 +50,11 @@ int perfect_aux(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int *p, full = 1;
+
+	p = &full;
 	if (!tree)
 		return (0);
 
-	return (perfect_aux(tree->left) == perfect_aux(tree->right));
+	return (perfect_aux(tree->left, &p) == perfect_aux(tree->right, &p));
 }
