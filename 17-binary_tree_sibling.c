7cf8facde29f5bb25ffe5ec7ee92f4b170c9ea8a
balemansteve <balemansteve@gmail.com>
2024-04-10 15:13:20 +0000

    binary tree silbing / header update

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+/*
+ * File: 17-binary_tree_sibling.c
+ * Auth: Gabriel Morffe, Bryan Aleman
+ */
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: is a pointer to the node to find the sibling
+ *
+ * Return: Silbing node or if node or the parent is NULL
+ * return NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
