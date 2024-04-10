9c2626ff9d5614d3d56f40f598d2e2192338d544
balemansteve <balemansteve@gmail.com>
2024-04-10 16:21:31 +0000

    binary tree uncle / header update

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,29 @@
+/*
+ * File: 18-binary_tree_uncle.c
+ * Auth: Gabriel Morffe, Bryan Aleman
+ */
+
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds the uncle of a node
+ * @node: is a pointer to the node to find the uncle
+ *
+ * Return: NULL if node is NULL, NULL if
+ * node has no uncle
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *grandfather;
+
+	grandfather = node->parent->parent;
+
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	if (grandfather->right == node->parent)
+		return (grandfather->left);
+
+	else
+		return (grandfather->right);
+}
