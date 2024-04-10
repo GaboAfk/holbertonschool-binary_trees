206a74aa0973557c9f30d97c86226862e08a43bd
GaboAfk <gabrielmorffe95@gmail.com>
2024-04-10 18:07:05 -0300

    recursive principaÃl function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -8,31 +8,22 @@
 /**
  * perfect_aux - checks if a binary tree is perfect
  * @tree: is a pointer to the root node of the tree to check
- * @full: flag of each full tree
  * Return: 1 tree is perfectf, else 0, if tree is NULL, return 0
  */
-int perfect_aux(const binary_tree_t *tree, int **full)
+int perfect_aux(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
 
 	if (!tree)
 		return (0);
 
-	if ((!tree->left && tree->right) || (tree->left && !tree->right))
-	{
-		**full = 0;
-		return (0);
-	}
-
 	if (tree->left)
-		left += 1 + perfect_aux(tree->left, full);
+		left += 1 + perfect_aux(tree->left);
 
 	if (tree->right)
-		right += 1 + perfect_aux(tree->right, full);
+		right += 1 + perfect_aux(tree->right);
 
-	if (**full == 1)
-		return (left > right ? left : right);
-	return (0);
+	return (left > right ? left : right);
 }
 
 /*
@@ -50,16 +41,19 @@ int perfect_aux(const binary_tree_t *tree, int **full)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int *pl, *pr, full_left = 1, full_right = 1, height_left, height_right;
-
-	pl = &full_left;
-	pr = &full_right;
+	int height_left, height_right;
 
 	if (!tree)
+		return (1);
+
+	height_left = perfect_aux(tree->left);
+	height_right = perfect_aux(tree->right);
+
+	if (height_left != height_right)
 		return (0);
 
-	height_left = perfect_aux(tree->left, &pl);
-	height_right = perfect_aux(tree->right, &pr);
-
-	return (*pl && *pr ? height_left == height_right : 0);
+if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+	return (1);
+	else
+		return (0);
 }
